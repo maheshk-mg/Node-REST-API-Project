c2014fdd5007186110a37a3ce66dc76e5fb93723
Mahesh K <mahesh.lanetteam@gmail.com>
2026-01-15 18:12:35 +0530

    Created pure CRUD of post feed

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
+
+const path = require("path");
+
+const MONGODB_URI =
+  "mongodb+srv://kshirsagarm773_db_user:mL6ouCSOffjsLZRw@cluster0.mfotn3y.mongodb.net/BlogPost?appName=Cluster0";
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
 
-// app.use(bodyParser.urlencoded());
-app.use(bodyParser.json()); //application
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
+
+const fileFiler = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/webp"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(bodyParser.json());
+
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFiler }).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -20,4 +53,20 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+  const status = (error.statusCode = 500);
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
+const PORT = 8000;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    console.log(`Connected.... to Server ${PORT}`);
+    app.listen(PORT);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- routes/feed.js
+++ routes/feed.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { body } = require("express-validator");
 
 const feedController = require("../controllers/feed");
 
@@ -8,6 +9,29 @@ const router = express();
 router.get("/posts", feedController.getPost);
 
 //POST /feed/post
-router.post("/post", feedController.postPost);
+router.post(
+  "/post",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.createPost
+);
+
+// GET POST by ID
+router.get("/post/:postId", feedController.getPostById);
+
+// PUT by ID
+router.put(
+  "/post/:postId",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.updatePost
+);
+
+//DELETE by ID
+router.delete("/post/:postId", feedController.deletPost);
 
 module.exports = router;
